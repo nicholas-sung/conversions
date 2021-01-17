9307c2a77bce9ce898adbb4552aa6fdab74be656
Nicholas Sung <nsung@hks.harvard.edu>
2021-01-17 17:16:29 -0500

    loaded json successfully, created variable data, opened output.csv successfully

--- example2/convert.py
+++ example2/convert.py
@@ -4,10 +4,17 @@ import json
 import csv
 
 # use json.load to load input.json. load it into a variable called 'data'
+with open('input.json', 'r') as f:
+    data = json.load(f)
 
 # use open to open 'output.csv' in write mode
+with open('test.csv', 'w') as f:
+
 # use csv.writer to create writer using the opened file
+	writer = csv.writer(f)
+
 # write the header of the csv
+	writer.writerow(['id', 'value'])
 
 # loop through the top level children data['children']
 # use writer.writerow to write one row at a time with the name and budget
